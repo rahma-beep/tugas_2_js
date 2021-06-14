523b4df64e91276eda050fa6cc279004352ee8e5
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-14 08:23:07 +0800

    TUGAS 2 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+var jarak = 8;
+var kendaraan = "yamaha";
+var  motor = true;
+var jenis = "matic";
+jenis = null;
+var ganjil = [1,3,5,7,9];
+
+console.log(jarak);
+console.log(kendaraan);
+console.log(motor);
+console.log(jenis);
+console.log(ganjil);
